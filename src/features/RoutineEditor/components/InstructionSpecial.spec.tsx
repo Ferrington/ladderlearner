33444d3b4034f69ec93c2b092d62780d3ba4f2ff
Christopher Bandurski <cbandurski1@gmail.com>
2024-02-16 10:38:38 -0600

    added playwright

--- src/features/RoutineEditor/components/InstructionSpecial.spec.tsx
+++ src/features/RoutineEditor/components/InstructionSpecial.spec.tsx
@@ -1,62 +1,69 @@
 import InstructionSpecial from '@/features/RoutineEditor/components/InstructionSpecial';
-import { RoutineSlice } from '@/store/routine/slice';
-import { TagSlice } from '@/store/tag/slice';
 import { renderWithProviders } from '@/test/renderWithProviders';
 import { screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
+import { RoutineSlice } from '@/store/routine/slice';
+import { TagSlice } from '@/store/tag/slice';
 import { userEvent } from '@testing-library/user-event';
+
+type TestState = {
+  routine: RoutineSlice;
+  tags: TagSlice;
+};
+
+const preloadedState: TestState = {
+  routine: {
+    rungs: {
+      allIds: ['rung1'],
+      byId: {
+        rung1: {
+          id: 'rung1',
+          type: 'Rung',
+          child: 'branch1',
+          comment: 'testComment',
+        },
+      },
+    },
+    branches: {
+      branch1: {
+        id: 'branch1',
+        type: 'AND',
+        parent: 'rung1',
+        children: ['test'],
+      },
+    },
+    instructions: {
+      instruction1: {
+        id: 'instruction1',
+        type: 'Instruction',
+        displayType: 'Special',
+        abbreviated: 'XIC',
+        tag: 'testTag',
+        parent: 'branch1',
+        isDestructive: false,
+        energized: false,
+      },
+    },
+  },
+  tags: {
+    allIds: ['testTag'],
+    byId: {
+      testTag: {
+        name: 'testTag',
+        type: 'bool',
+        value: false,
+      },
+    },
+  },
+};
 
 describe('InstructionSpecial', () => {
   it('should autocomplete a tag name', async () => {
     const user = userEvent.setup();
-    const routineState: RoutineSlice = {
-      rungs: {
-        allIds: ['rung1'],
-        byId: {
-          rung1: {
-            id: 'rung1',
-            type: 'Rung',
-            child: 'branch1',
-            comment: 'testComment',
-          },
-        },
-      },
-      branches: {
-        branch1: {
-          id: 'branch1',
-          type: 'AND',
-          parent: 'rung1',
-          children: ['test'],
-        },
-      },
-      instructions: {
-        instruction1: {
-          id: 'instruction1',
-          type: 'Instruction',
-          displayType: 'Special',
-          abbreviated: 'XIC',
-          tag: 'testTag',
-          parent: 'branch1',
-          isDestructive: false,
-          energized: false,
-        },
-      },
-    };
-
-    const tagState: TagSlice = {
-      allIds: ['testTag'],
-      byId: {
-        testTag: {
-          name: 'testTag',
-          type: 'bool',
-          value: false,
-        },
-      },
-    };
 
     renderWithProviders(<InstructionSpecial instructionId="instruction1" beingDragged={false} />, {
-      preloadedState: { routine: routineState, tags: tagState },
+      preloadedState,
     });
 
     const instruction = screen.getByTestId('instruction-special');
